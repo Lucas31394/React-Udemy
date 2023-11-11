5c18572c5bc8249a78c8693d36097d3d9e789cfc
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2023-11-11 15:25:58 -0300

    learning about conditionals in jsx

--- 3_advancingInReact/src/components/ConditionalRenderer.jsx
+++ 3_advancingInReact/src/components/ConditionalRenderer.jsx
@@ -4,11 +4,31 @@ const ConditionalRenderer = () => {
     const [x] = useState(true);
     const [y] = useState(false);
 
+    const [name, setName] = useState("John");
+
+    const [newName, setSecondName] = useState("Will")
+
+    const ReplaceName = (firstName, secondName) => {
+      setName(secondName);
+      setSecondName(firstName);
+    }
+
   return (
     <div>
         <h1>Would it be shown?</h1>
         {x && <p>If x is true, this will be shown</p>}
         {!y && <p>If y is false, this will be shown</p>}
+        <h1>Testing the if in JSX</h1>
+        {name === "John" ? (
+          <div>
+            <p>The name is John</p>
+          </div>
+        ) : (
+          <div>
+            <p>The name is {name}</p>
+          </div>
+        )}
+        <button onClick={() => ReplaceName(name, newName)}>Change the name!</button>
     </div>
   )
 }
