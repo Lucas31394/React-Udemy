7b4af135a20d4efef90faf15dc01cf6bfd79b159
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2023-11-11 09:50:30 -0300

    key property

--- 3_advancingInReact/src/components/ListRenderer.jsx
+++ 3_advancingInReact/src/components/ListRenderer.jsx
@@ -4,12 +4,24 @@ const ListRenderer = () => {
 
     const [list] = useState(["Lucas", "Thiago", "Gabriel", "Pedro"])
 
+    const [users] = useState([
+      {id: 1, name: "Lucas", age: 29},
+      {id: 2, name: "Thiago", age: 28},
+      {id: 3, name: "Gabriel", age: 25},
+      {id: 4, name: "Pedro", age: 23}
+    ])
+
   return (
     <div>
         <ul>
-            {list.map((item) => (
-                <li>{item}</li>
+            {list.map((item, i) => (
+                <li key={i}>{item}</li>
             ))}
+        </ul>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name} - {user.age}</li>
+          ))}
         </ul>
     </div>
   )
