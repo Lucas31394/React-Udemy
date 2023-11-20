7f47f5d30da22306de2a086439264c83f46f44bd
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2023-11-20 12:04:08 -0300

    dynamic inline css

--- 4_react_css/src/App.jsx
+++ 4_react_css/src/App.jsx
@@ -5,7 +5,9 @@ import './App.css'
 import MyComponent from './components/MyComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const n = 15;
+  const [name] = useState("Lucas"); 
 
   return (
     <>
@@ -17,6 +19,10 @@ function App() {
         <p>This is the App.jsx paragraph.</p>
         {/*Inline CSS*/}
         <p style={{color: "blue", padding: "25px", borderTop: "2px solid red"}}>This element has the style attribute inline.</p>
+        {/*Dynamic inline CSS*/}
+        <h2 style={n < 10 ?   ({color: "purple"}) : ({color: "pink"})}>Dynamic CSS</h2>
+        <h2 style={n > 10 ?   ({color: "purple"}) : ({color: "pink"})}>Dynamic CSS</h2>
+        <h2 style={name === "Lucas" ?   ({color: "green", backgroundColor: "#000"}) : null}>Dynamic CSS</h2>
       </div>
     </>
   )
