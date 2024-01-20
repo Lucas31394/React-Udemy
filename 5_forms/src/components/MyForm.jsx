0098b80f0bb472d57c73ed5a559c68e81f96fa4f
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2024-01-20 12:06:01 -0300

    data management

--- 5_forms/src/components/MyForm.jsx
+++ 5_forms/src/components/MyForm.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './MyForm.css'
 
 const MyForm = () => {
+    //Data management
+    const[name, setName] = useState()
+    const[email, setEmail] = useState()
+
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+
+    console.log(name)
+
   return (
     <div>
         {/* form creation */}
         <form>
             <div>
                 <label htmlFor="name">Name: </label>
-                <input type="text" name="name" placeholder="Type your name."/>
+                <input type="text" name="name" placeholder="Type your name." onChange={handleName}/>
             </div>
             {/* tag input inside label tag */}
             <label>
