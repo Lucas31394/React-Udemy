54b83d546a3541717fe9462ebaf5daea6b9f5a2a
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2024-01-20 11:57:36 -0300

    using the tag label surrounding the tag input

--- 5_forms/src/components/MyForm.jsx
+++ 5_forms/src/components/MyForm.jsx
@@ -10,6 +10,11 @@ const MyForm = () => {
                 <label htmlFor="name">Name: </label>
                 <input type="text" name="name" placeholder="Type your name."/>
             </div>
+            {/* tag input inside label tag */}
+            <label>
+                <span>Email: </span>
+                <input type="text" name="email" placeholder="Type your e-mail."/>
+            </label>
             <input type="submit" value="Send"/>
         </form>
     </div>
