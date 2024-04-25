df64d8e92822f52caa4469ad22054f0a0e3bb5f8
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2024-04-25 11:05:10 -0300

    structuring the game component

--- 6_project-secret-word/src/components/Game.jsx
+++ 6_project-secret-word/src/components/Game.jsx
@@ -2,9 +2,30 @@ import './Game.css';
 
 const Game = ({verify}) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verify}>Check!</button>
+    <div className="game">
+      <p className="points">
+        <span>Points: 000</span>
+      </p>
+      <h1>Guess the word</h1>
+      <h3 className="tip">
+        Tip about the word: <span>Tip...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className='letter'>A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>try to guess a letter: </p>
+        <form>
+          <input type="text" name='letter' maxLength={1} required />
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letters used: </p>
+        <span>a, </span>
+        <span>b, </span>
+      </div>
     </div>
   )
 }
