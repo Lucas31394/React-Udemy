e998d40a5a1395aadce5677095434218c8660ab0
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2024-05-29 13:20:36 -0300

    user registration

--- 12_reactgram/backend/controllers/UserController.js
+++ 12_reactgram/backend/controllers/UserController.js
@@ -14,7 +14,36 @@ const generateToken = (id) => {
 
 // Register user and sign in
 const register = async(req, res) => {
-    res.send("Registered.");
+    const {name, email, password} = req.body;
+
+    // check if the user exists
+    const user = await User.findOne({email});
+
+    if(user) {
+        res.status(422).json({errors: ["Please, insert a different email."]});
+        return;
+    }
+
+    // Generate password hash
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // Create user
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash
+    });
+
+    // If user was created successfully, return the token
+    if(!newUser) {
+        res.status(422).json({errors: ["An error ocurred while trying to end the registration, please try again latter."]});
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id)
+    })
 };
 
 module.exports = {
