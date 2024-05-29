53984c9d96b650b411cfaa6dbdcf43f37ce091c8
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2024-05-28 21:19:22 -0300

    setting the db connection

--- 12_reactgram/backend/config/db.js
+++ 12_reactgram/backend/config/db.js
@@ -1,3 +1,17 @@
 const mongoose = require("mongoose");
+const dbUser = process.env.DB_USER;
+const dpPassword = process.env.DB_PASS;
 
-// connection
+const conn = async() => {
+    try {
+        const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dpPassword}@cluster0.ykcgasr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+
+        console.log("Connection stablished!");
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+conn();
+
+module.exports = conn;
