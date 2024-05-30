9e469f24dd3357cb611cbaf1b72d2770eab9d2f1
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2024-05-30 10:40:36 -0300

    initial setup for photo routes

---
+++ 12_reactgram/backend/routes/PhotoRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+// Controller
+
+// Middleware
+const { photoInsertValidation } = require("../middlewares/photoValidation");
+const authGuard = require("../middlewares/authGuard");
+const validate = require("../middlewares/handleValidation");
+// Routes
+
+module.exports = router;
