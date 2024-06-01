21a22b692b82678b0cef7be7fe0934f8a5287ac6
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2024-06-01 11:27:26 -0300

    setting react router

--- 12_reactgram/frontend/src/App.jsx
+++ 12_reactgram/frontend/src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+
+// Router
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+
+// Page
+import Home from "./pages/Home/Home";
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <h1>ReactGram</h1>
+      <div className='App'>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/register" element={<Register />}></Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   )
