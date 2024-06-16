cf7921ec95a349fe72fa1ddb4390123fdded3071
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2024-06-16 12:16:18 -0300

    logout function

--- 12_reactgram/frontend/src/slices/authSlice.jsx
+++ 12_reactgram/frontend/src/slices/authSlice.jsx
@@ -22,6 +22,11 @@ export const register = createAsyncThunk("auth/register", async(user, thunkAPI) 
     return data;    
 });
 
+// Logout an user
+export const logout = createAsyncThunk("auth/logout", async() => {
+    await authService.logout();
+})
+
 export const authSlice = createSlice({
     name: "auth",
     initialState,
@@ -44,6 +49,11 @@ export const authSlice = createSlice({
         }).addCase(register.rejected, (state, action) => {
             state.loading = false;
             state.error = action.payload;
+            state.user = null;
+        }).addCase(logout.fulfilled, (state, action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null;
             state.user = null;
         })
     }
