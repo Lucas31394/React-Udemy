d2f117cebdc4a827206927d07fcd38960512567d
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2024-07-28 10:42:27 -0300

    delete photo slice

--- 12_reactgram/frontend/src/slices/photoSlice.jsx
+++ 12_reactgram/frontend/src/slices/photoSlice.jsx
@@ -9,7 +9,7 @@ const initialState = {
     success: false,
     loading: false,
     message: null
-}
+};
 
 // Publish user photo
 export const publishPhoto = createAsyncThunk(
@@ -25,7 +25,7 @@ export const publishPhoto = createAsyncThunk(
 
         return data;
     }
-)
+);
 
 // Get user photos
 export const getUserPhotos = createAsyncThunk(
@@ -34,6 +34,22 @@ export const getUserPhotos = createAsyncThunk(
         const token = thunkAPI.getState().auth.user.token;
 
         const data = await photoService.getUserPhotos(id, token);
+
+        return data;
+    }
+);
+
+// Delete photo
+export const deletePhoto = createAsyncThunk(
+    "photo/delete",
+    async(id, thunkAPI) => {
+        const token = thunkAPI.getState().auth.user.token;
+
+        const data = await photoService.deletePhoto(id, token);
+
+        if(data.errors) {
+            return thunkAPI.rejectWithValue(data.errors[0]);
+        }
 
         return data;
     }
@@ -74,6 +90,24 @@ export const photoSlice = createSlice({
             state.success = true;
             state.error = null;
             state.photos = action.payload;
+        })
+        .addCase(deletePhoto.pending, (state) => {
+            state.loading = true;
+            state.error = false;
+        })
+        .addCase(deletePhoto.fulfilled, (state, action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+            state.photos = state.photos.filter((photo) => {
+                return photo._id !== action.payload.id
+            })
+            state.message = action.payload.message;
+        })
+        .addCase(deletePhoto.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+            state.photo = {};
         })
     }
 });
