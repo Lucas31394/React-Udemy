7058ea78245df8931400681eb3dbe9fc290f505f
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2024-09-01 13:55:06 -0300

    photo slice by id

--- 12_reactgram/frontend/src/slices/photoSlice.jsx
+++ 12_reactgram/frontend/src/slices/photoSlice.jsx
@@ -69,6 +69,16 @@ export const updatePhoto = createAsyncThunk(
 
         return data;
     }
+);
+
+// Get photo by id
+export const getPhoto = createAsyncThunk(
+    "photo/getPhoto",
+    async(id) => {
+        const data = await photoService.getPhoto(id);
+
+        return data;
+    }
 )
 
 export const photoSlice = createSlice({
@@ -145,6 +155,16 @@ export const photoSlice = createSlice({
             state.loading = false;
             state.error = action.payload;
             state.photo = {};
+        })
+        .addCase(getPhoto.pending, (state) => {
+            state.loading = true;
+            state.error = false;
+        })
+        .addCase(getPhoto.fulfilled, (state, action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+            state.photo = action.payload;
         })
     }
 });
