d3e3f30aadedde47406c50c9f1e904a62fc74a27
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2024-09-01 14:13:25 -0300

    showing the image and data

--- 12_reactgram/frontend/src/slices/photoSlice.jsx
+++ 12_reactgram/frontend/src/slices/photoSlice.jsx
@@ -74,8 +74,10 @@ export const updatePhoto = createAsyncThunk(
 // Get photo by id
 export const getPhoto = createAsyncThunk(
     "photo/getPhoto",
-    async(id) => {
-        const data = await photoService.getPhoto(id);
+    async(id, thunkAPI) => {
+        const token = thunkAPI.getState().auth.user.token;
+
+        const data = await photoService.getPhoto(id, token);
 
         return data;
     }
